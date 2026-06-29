de1950b0fd68e4dc1147943a259eabc1d8ae34e5
Brandon Kirincich <Brandon.Kirincich@amd.com>
2026-06-29 10:13:47 -0700

    autoperf: session baseline checkpoint (BEST)

--- script/test/envlocal.sh
+++ script/test/envlocal.sh
@@ -5,8 +5,9 @@ export BOARDNAME=palmyra-32
 # export VEK385IP=portobello13
 # export VEK385IP=crimini2
 # export VEK385IP=crimini5
-export VEK385IP=portobello13
+# export VEK385IP=portobello13
 # export VEK385IP=gomphus8
+export VEK385IP=crimini4   # vek385-14
 
 
 
